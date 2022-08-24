40418bc0383afc2759528ac53afbadcdbe22a404
sheng yeong <chia.shengyeong@gmail.com>
2022-08-24 16:39:33 +0800

    added styling with module.css

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import classes from "./event-item.module.css"
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -10,23 +11,25 @@ function EventItem(props) {
   const formattedAddress = location.replace(', ', '\n')
   const exploreLink = `/events/${id}`
 
-  return <li>
-    <img src={"/" + image} alt={title} />
-    <div>
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
-        </div>
-        <div>
-          <address>{formattedAddress}</address>
-        </div>
-        <div>
-          <Link href={exploreLink}>Explore event</Link>
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
+          <div className={classes.actions}>
+            <Link href={exploreLink}>Explore event</Link>
+          </div>
         </div>
       </div>
-    </div>
-  </li>
+    </li>
+  )
 }
 
 export default EventItem
